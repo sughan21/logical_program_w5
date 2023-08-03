af16b6f0ef9d7566eec230f511041a7652fe7064
sughan <sughanrithvik1@gmail.com>
2023-08-03 08:27:02 +0530

    logical_program_w5

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1.Create a simple calculator that takes two numbers and an operator (+, -, *, or /) as input and displays the result when a "Calculate" button is clicked.
+
+//2. Create a random quote generator that displays a new quote from an array of quotes when a "Generate Quote" button is clicked.
+
+//3. Create a form with name and email fields. Display an error message if the name is empty or the email format is invalid when a "Submit" button is clicked.
+
+//4. Create a typing speed tester that displays a random sentence and calculates the typing speed (words per minute) when the user finishes typing.
